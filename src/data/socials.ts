d0c8159c1ed931bc0ef87afb765424c4b1ac89f1
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-11-13 14:03:09 -0800

    first commit

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,26 +2,27 @@ import IconDribbble from "../components/icons/dribbble.astro";
 import IconGithub from "../components/icons/github.astro";
 import IconLinkedin from "../components/icons/linkedin.astro";
 import IconTwitter from "../components/icons/twitter.astro";
+import IconBluesky from "../components/icons/bluesky.astro";
 
 export const SOCIALS = [
   {
     name: "Github",
-    url: "https://github.com/thevrus",
+    url: "https://github.com/rcm-webdev",
     icon: IconGithub,
   },
   {
     name: "Linkedin",
-    url: "https://www.linkedin.com/in/vrusin/",
+    url: "https://www.linkedin.com/in/roberto-c-murillo/",
     icon: IconLinkedin,
   },
   {
     name: "Twitter",
-    url: "https://twitter.com/rusin_vadim",
+    url: "https://x.com/rcmwdev",
     icon: IconTwitter,
   },
   {
-    name: "Dribbble",
-    url: "https://dribbble.com/iamvr",
-    icon: IconDribbble,
+    name: "Bluesky",
+    url: "https://bsky.app/profile/rcmcodes.bsky.social",
+    icon: IconBluesky,
   },
 ];
